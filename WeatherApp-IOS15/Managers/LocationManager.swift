730f7c99a9c8d9b98dc27b6b7b95a36da08b9be8
danielgarofalo <garofalodaniel@gmail.com>
2022-01-23 13:46:10 -0600

    Location Manager added

---
+++ WeatherApp-IOS15/Managers/LocationManager.swift
@@ -0,0 +1,54 @@
+//
+//  LocationManager.swift
+//  WeatherApp-IOS15
+//
+//  Created by Daniel Garofalo on 1/23/22.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager : NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    //MARK: - PROPERTIES
+    
+    //manager location
+    let manager = CLLocationManager()
+    
+    //Optional location. Might not get a location (
+    @Published var location : CLLocationCoordinate2D?
+    //Loading State
+    @Published var isLoading = false
+    
+    
+    //MARK: - CUSTOM METHODS
+    
+    //init of class
+    override init() {
+        super.init()
+        
+        manager.delegate = self
+    }
+    
+    //request location
+    func requestLocation(){
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        //save the coordinates into the location and toggle the isLoading flag
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    //Exception handler
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+        print("Error getting location: ", error)
+        isLoading = false
+    }
+    
+}
