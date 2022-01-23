37f669f8fdaff3de1a0143ed2933b6f766e1a15e
danielgarofalo <garofalodaniel@gmail.com>
2022-01-23 13:04:58 -0600

    Initial Commit

---
+++ WeatherApp-IOS15/WeatherApp_IOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_IOS15App.swift
+//  WeatherApp-IOS15
+//
+//  Created by Daniel Garofalo on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_IOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
