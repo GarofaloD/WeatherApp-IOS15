3ae1c40dd6261e97f788ea2d81c042044c6d7969
danielgarofalo <garofalodaniel@gmail.com>
2022-02-21 11:50:03 -0600

    Full ui completed to display weather

--- WeatherApp-IOS15/Views/WeatherView.swift
+++ WeatherApp-IOS15/Views/WeatherView.swift
@@ -20,7 +20,94 @@ struct WeatherView: View {
     //MARK: - BODY
     //MARK: VIEW
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //ZSTack
+        ZStack(alignment: .leading){
+            
+            //Full container
+            VStack{
+                //Top Stack: City and Date
+                VStack(alignment: .leading, spacing: 5){
+                    Text(weather.name)
+                        .bold().font(.title)
+                    Text("Today: \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                }//:VStack
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                //Mid Stack: City/main temp and main indicator
+                VStack {
+                    HStack{
+                        VStack(spacing: 20){
+                            Image(systemName: "cloud.fill")
+                                .font(.system(size: 80))
+                            
+                            Text(weather.weather[0].main)
+                        }
+                        .frame(width: 150, alignment: .leading)
+                        
+                        Spacer()
+                        
+                        Text(weather.main.feelsLike.roundDouble() + "°")
+                            .font(.system(size: 80))
+                            .fontWeight(.bold)
+                            .padding()
+                    }
+                    
+                    Spacer()
+                        .frame(height: 80)
+                    
+                    //City image
+                    AsyncImage(url:URL(string:"https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png")) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 350)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    
+                    Spacer()
+                }//:VStack
+                .frame(maxWidth: .infinity)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            //Bottom Stack -  Weather now (weather details)
+            VStack{
+                Spacer()
+                VStack(alignment: .leading, spacing: 20){
+                    Text("Weather Now:")
+                        .bold().padding(.bottom)
+                    
+                    HStack{
+                        WeatherRow(logo: "thermometer", name: "Min Temp", value: (weather.main.tempMin.roundDouble() + "°"))
+                        Spacer()
+                        WeatherRow(logo: "thermometer", name: "Max Temp", value: (weather.main.tempMax.roundDouble() + "°"))
+                    }
+                    HStack{
+                        WeatherRow(logo: "wind", name: "Wind Speed", value: (weather.wind.speed.roundDouble() + "m/s"))
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Humidity", value: (weather.main.humidity.roundDouble() + "%"))
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }//:VStack
+            
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(.dark)
+        
+        
+        
     }//:VIEW
 }//:BODY
 
